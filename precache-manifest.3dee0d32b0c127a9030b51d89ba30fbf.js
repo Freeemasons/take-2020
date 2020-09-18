4ac7aadc6aff7d455d059b5aa6f433240c2b7ba9
freemasons <shivasmile11@gmail.com>
2020-09-18 05:14:01 +0300

    Updates

---
+++ precache-manifest.3dee0d32b0c127a9030b51d89ba30fbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439a2e0023964c590b90e8faeab4322a",
+    "url": "/take-2020/index.html"
+  },
+  {
+    "revision": "c574d268fd219da2ead1",
+    "url": "/take-2020/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "2f783535fbaaf82a3d17",
+    "url": "/take-2020/static/css/main.dcc71d00.chunk.css"
+  },
+  {
+    "revision": "c574d268fd219da2ead1",
+    "url": "/take-2020/static/js/2.28c8de84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/take-2020/static/js/2.28c8de84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f783535fbaaf82a3d17",
+    "url": "/take-2020/static/js/main.033343d0.chunk.js"
+  },
+  {
+    "revision": "1f84e2e3799f7a2ebda2",
+    "url": "/take-2020/static/js/runtime-main.fb857a4c.js"
+  }
+]);
